b5fd0324e28f2beca5972b3535844550a061900b
nicchoku-teacher <endoh-k@edu-g.gsn.ed.jp>
2025-06-01 17:18:17 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NicchokuApp from './NicchokuApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <NicchokuApp />
+  </React.StrictMode>,
+)
