d91d513b9afa923fe482181d52a98cde90f943a0
nicchoku-teacher <endoh-k@edu-g.gsn.ed.jp>
2025-06-02 10:53:50 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import NicchokuApp from './NicchokuApp'
-import './index.css'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NicchokuApp />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
